23fae3dfafc0e5f5e3accbff4fa8fda1bafca7a6
azureuz <chinmayshekatkar12@gmail.com>
2023-01-14 14:29:01 +0900

    bscscan api integration

---
+++ src/README.md
@@ -0,0 +1,32 @@
+# BSCSCAN API
+
+# Pre-requisites
+- Install [Node.js](https://nodejs.org/en/) version 16.14.2
+- Install [npm] version 8.5.0
+
+# Getting started
+- Clone the repository
+```
+git clone  <git lab template url> <project_name>
+```
+- Install dependencies
+```
+cd <project_name>
+npm install
+```
+- Navigate to dist/ folder
+```
+cd dist/
+```
+- Update the API key for bsc scan in .env file
+
+- Build the project
+```
+npm run build
+```
+- Run the project
+```
+node . <wallet-address> 
+example - node . 0x7Fb9d6760fafD3E7998172887E6f9dFE6917FaFa
+```
+
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios';
+import { ethers } from "ethers";
+
+dotenv.config()
+const apiKey = process.env.API_KEY; //bscscan API key added in /dist/.env file
+
+let map = {};
+
+/**
+ * Converts UNIX Timestamp to Local time
+ * @param - UNIX Timestap
+ * @return - Formatted Local time
+ */
+function timeConverter(UNIX_timestamp) {
+  var convertedDate = new Date(UNIX_timestamp * 1000);
+  var year = convertedDate.getFullYear();
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var monthName = month[convertedDate.getMonth()];
+  var date = convertedDate.getDate();
+  var hours = convertedDate.getHours();
+  var minutes = "0" + convertedDate.getMinutes();
+  var seconds = "0" + convertedDate.getSeconds();
+  var formattedTime = date + ' ' + monthName + ' ' + year + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  return formattedTime;
+}
+
+/**
+ * Gets Token transactions and wallet balance
+ */
+async function getData() {
+  try {
+    const walletAddress = process.argv.slice(2); //provided wallet address in CLI argument
+    
+    //checks if provided CLI argument wallet address is correct using Ethers library 
+    const index = walletAddress.findIndex(item => ethers.utils.isAddress(item) ?
+      console.log("Getting data for following wallet: ", item)
+      : console.log("Invalid Address")
+    );
+
+    //API request to bscscan with cli argument passed in
+    const response = await axios.get(`https://api.bscscan.com/api?module=account&action=tokentx&address=${process.argv.slice(2)}&startblock=0&endblock=999999999&sort=asc&apikey=${apiKey}`);
+    console.log();
+    console.log("Token Transactions");
+    response.data.result.forEach(function (element: any) {
+      console.log("------------------------------------------------------------------------------");
+      console.log("Transaction ID: ", element.hash);
+      console.log("Date & Time: ", timeConverter(element.timeStamp)); //convert Unix time to readable format
+      console.log("Token Symbol: ", element.tokenSymbol);
+      console.log("Amount: ", ethers.utils.formatEther(element.value)); //format token to 18 decimals using Ethers library
+      map[element.tokenSymbol] = element.contractAddress //using hashmap to get all tokens list
+      console.log("------------------------------------------------------------------------------", "\n");
+    });
+    console.log("BEP20 Assets and their contract addresses", map);
+    console.log();
+
+    //Get user balance for each of the above tokens by calling bscscan API
+    for (let key in map) {
+      console.log(`User balance for the ${key} Token`);
+      console.log("----------------------------------------");
+      const response = await axios.get(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${map[key]}&address=${process.argv.slice(2)}&tag=latest&apikey=${apiKey}`);
+      let balance = response.data.result;
+      const tokenValue = ethers.utils.formatEther(balance);
+      console.log(tokenValue);
+      console.log("----------------------------------------", "\n");
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
